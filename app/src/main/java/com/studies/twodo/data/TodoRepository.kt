03cd9667c8a19575da4b708fdc02d3a99143f604
Davy de Souza <davydesouzabar@gmail.com>
2025-08-25 12:03:16 -0300

    feat: create todo repository

---
+++ app/src/main/java/com/studies/twodo/data/TodoRepository.kt
@@ -0,0 +1,15 @@
+package com.studies.twodo.data
+import com.studies.twodo.domain.Todo
+import kotlinx.coroutines.flow.Flow
+
+interface TodoRepository {
+    suspend fun insert(title: String, description: String?)
+
+    suspend fun updateIsFinished(id: Long, isFinished: Boolean)
+
+    suspend fun delete(id: Long)
+
+    fun getAll(): Flow<List<Todo>>
+
+    suspend fun getById(id: Long): Todo?
+}
---
+++ app/src/main/java/com/studies/twodo/data/TodoRepositoryImpl.kt
@@ -0,0 +1,56 @@
+package com.studies.twodo.data
+
+import com.studies.twodo.domain.Todo
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class TodoRepositoryImpl(
+    private val dao: TodoDAO
+) : TodoRepository {
+   override suspend fun insert(title: String, description: String?){
+        val entity = TodoEntity(
+            description = description,
+            isFinished = false,
+            title = title,
+        )
+
+        dao.insert(entity)
+    }
+
+    override suspend fun updateIsFinished(id: Long, isFinished: Boolean){
+        val entityFound = dao.getById(id)
+        if(entityFound === null) return;
+
+        val updatedEntity = entityFound.copy(isFinished = isFinished)
+        dao.insert(updatedEntity)
+    }
+
+    override suspend fun delete(id: Long){
+        val entityFound = dao.getById(id)
+        if(entityFound === null) return;
+        dao.delete(entityFound)
+    }
+
+    override fun getAll(): Flow<List<Todo>> {
+        return dao.getAll().map {
+            it.map { entity ->
+                mapTodoEntity(entity)
+            }
+        }
+    }
+
+    override suspend fun getById(id: Long): Todo? {
+        return dao.getById(id)?.let { entity ->
+            mapTodoEntity(entity)
+        }
+    }
+
+    private fun mapTodoEntity(entity: TodoEntity): Todo {
+       return Todo(
+            id = entity.id,
+            title = entity.title,
+            description = entity.description,
+            isFinished = entity.isFinished
+        )
+    }
+}
